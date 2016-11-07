85b68be8b25e1fb79252cda99ca9344379eaf0ab
Enrique P <ername@users.noreply.github.com>
2016-11-07 11:50:13 -0600

    Added random

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -18,6 +18,29 @@ class CalculationsController < ApplicationController
     @sq=@id_number*@id_number
 
     render("square.html.erb")
+  end
+
+  def random
+    @id_1=params[:id_1]
+    @id_2=params[:id_2]
+    @id_number_1=@id_1.to_i
+    @id_number_2=@id_2.to_i
+    prng = Random.new
+    @random=prng.rand(@id_number_1..@id_number_2)
+    render("random.html.erb")
+  end
+
+  def payment
+    @id_1=params[:id_1]
+    @id_2=params[:id_2]
+    @id_3=params[:id_3]
+    @id_number_1=@id_1.to_f
+    @id_number_2=@id_2.to_i
+    @id_number_3=@id_3.to_i
+
+    render("payments.html.erb")
+
 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
   get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
-  get("/square/:id", { :controller => "calculations", :action => "square" }) 
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+  get("/random/:id_1/:id_2", { :controller => "calculations", :action => "random" })
+  get("/payment/:id_1/:id_2/:id_3", { :controller => "calculations", :action => "payment" })
 end
