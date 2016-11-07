89664cdf5b2ea6e6c11ea9de276b4c75d707a0d4
Enrique P <ername@users.noreply.github.com>
2016-11-07 11:03:38 -0600

    Square and sqrt

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,22 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+    @id=params[:id]
+    @id_number=@id.to_i
+    @sqrt=Math.sqrt(@id_number)
+
+    render("square_root.html.erb")
+
+  end
+
+  def square
+    @id=params[:id]
+    @id_number=@id.to_i
+    @sq=@id_number*@id_number
+
+    render("square.html.erb")
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+  get("/square/:id", { :controller => "calculations", :action => "square" }) 
 end
